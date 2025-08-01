1b3d152ca793abd3fb07de72e72b4c55622e72c5
Joseph Diza <josephm.diza@gmail.com>
2025-07-31 20:12:26 -0700

    Add Employees schema

--- src/models/schema.rs
+++ src/models/schema.rs
@@ -13,3 +13,22 @@ diesel::table! {
         email -> Nullable<Varchar>,
     }
 }
+
+diesel::table! {
+    employees (employee_id) {
+        employee_id -> Integer,
+        #[max_length = 50]
+        first_name -> Varchar,
+        #[max_length = 50]
+        last_name -> Varchar,
+        #[max_length = 12]
+        phone -> Varchar,
+        #[max_length = 50]
+        email -> Varchar,
+    }
+}
+
+diesel::allow_tables_to_appear_in_same_query!(
+    customers,
+    employees,
+);
